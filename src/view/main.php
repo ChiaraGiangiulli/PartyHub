1a26418be18b17df00c7d7832f1ea26b0062e92b
ChiaraGiangiulli <chiara.giangiulli@studio.unibo.it>
2024-01-17 15:16:53 +0100

    created folder view

---
+++ src/view/main.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>PartyHub</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+    <div class="container p-3 bg-dark text-white">
+        <div class="row">
+            <div class="col">
+                <div class="well">
+                    <center><h1>PartyHub</h1></center>
+                </div>
+            </div> 
+        </div>
+    </div>
+    <div class="container p-5 border border-4 border-dark">
+    <div class="container mt-3">
+  <h2>Login</h2>
+  <form action="/PartyHub/src/api/login.php" method="post">
+    <div class="mb-3 mt-3">
+      <label for="username">Username:</label>
+      <input type="text" class="form-control" id="username" placeholder="Username" name="username" required>
+    </div>
+    <div class="mb-3">
+      <label for="pwd">Password:</label>
+      <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="psw" required>
+    </div>
+    <button type="submit" class="btn btn-primary">Login</button>
+</form>
+</div>
+<form action="/PartyHub/src/formSignup.php">
+    <button type="submit" class="btn btn-link">Not a member? Signup.</button>
+    </form>
+
+</body>
+</html>
