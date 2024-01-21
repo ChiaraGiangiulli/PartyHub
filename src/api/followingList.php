af72ff069ed740e66c6b0673ab5e94b2131d8f3a
ChiaraGiangiulli <chiara.giangiulli@studio.unibo.it>
2024-01-21 11:13:01 +0100

    testing modals

--- src/api/followingList.php
+++ src/api/followingList.php
@@ -1,12 +1,26 @@
-<?php include '../db/query.php';
-
-$dbh = new DatabaseHelper("localhost", "root", "", "partyhub", 3306);
-
-session_start();
-$user = $_SESSION['user_id'];
-$following = $dbh->getFollowing($user);
-foreach ($following as $risultato) {
-    print_r($risultato['Following']);
-}
-
-?>
+<div class="modal fade" id="followingModal" role="dialog" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="following">Following</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <?php include '../db/query.php';
+            session_start();
+            $dbh = new DatabaseHelper("localhost", "root", "", "partyhub", 3306);
+            $user = $_SESSION['user_id'];
+            $following = $dbh->getFollowing($user);
+            foreach ($following as $risultato) {
+                print_r($risultato['Following']);
+            }
+        ?>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Chiudi</button>
+      </div>
+    </div>
+  </div>
+</div>
--- src/view/profile.php
+++ src/view/profile.php
@@ -1,6 +1,9 @@
 <?php require_once 'homeBar.php' ;
   session_start();
 ?>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 <script src="../js/profile.js" defer></script>
     <div class="col-sm-12 text-center mt-4">
       <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-person-bounding-box" viewBox="0 0 16 16">
@@ -12,12 +15,12 @@
       <?php print_r($_SESSION["user_id"]);?>
     </div>
     <div class="col-sm-12 text-center mt-4 mb-4">
-      <div class="btn-group">
-        <button id="following" type="button" class="btn btn-outline-success">
+      <div id="profileForm" class="btn-group">
+        <button id="following" type="button" class="btn btn-outline-success" data-bs-toggle="modal" data-bs-target="#followingModal">
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-people" viewBox="0 0 16 16">
             <path d="M15 14s1 0 1-1-1-4-5-4-5 3-5 4 1 1 1 1zm-7.978-1L7 12.996c.001-.264.167-1.03.76-1.72C8.312 10.629 9.282 10 11 10c1.717 0 2.687.63 3.24 1.276.593.69.758 1.457.76 1.72l-.008.002-.014.002zM11 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4m3-2a3 3 0 1 1-6 0 3 3 0 0 1 6 0M6.936 9.28a6 6 0 0 0-1.23-.247A7 7 0 0 0 5 9c-4 0-5 3-5 4q0 1 1 1h4.216A2.24 2.24 0 0 1 5 13c0-1.01.377-2.042 1.09-2.904.243-.294.526-.569.846-.816M4.92 10A5.5 5.5 0 0 0 4 13H1c0-.26.164-1.03.76-1.724.545-.636 1.492-1.256 3.16-1.275ZM1.5 5.5a3 3 0 1 1 6 0 3 3 0 0 1-6 0m3-2a2 2 0 1 0 0 4 2 2 0 0 0 0-4"/>
           </svg> Following</button>
-        <button id="followers" type="button" class="btn btn-outline-success">
+        <button id="followers" type="button" class="btn btn-outline-success" data-bs-toggle="modal" data-bs-target="#followersModal">
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-people-fill" viewBox="0 0 16 16">
             <path d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6m-5.784 6A2.24 2.24 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.3 6.3 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1zM4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5"/>
           </svg> Followers</button>
@@ -31,12 +34,11 @@
     <nav class="navbar navbar-expand bg-light justify-content-center">
         <ul class="navbar-nav">
           <li class="nav-item">
-            <a class="nav-link <?php if(basename($_SERVER['PHP_SELF']) == "/PartyHub/src/views/posts.php"){echo "active";}?>" href="/PartyHub/src/view/profilePost.php">Posts</a>
+            <a class="nav-link <?php if(basename($_SERVER['PHP_SELF']) == "/PartyHub/src/views/profilePosts.php"){echo "active";}?>" href="/PartyHub/src/view/profilePost.php">Posts</a>
           </li>
           <li class="nav-item">
             <a class="nav-link <?php if(basename($_SERVER['PHP_SELF']) == "/PartyHub/src/api/userEvents.php"){echo "active";}?>" href="/PartyHub/src/api/userEvents.php">Events</a>
           </li>
         </ul>
     </nav>
-  
 </body>
