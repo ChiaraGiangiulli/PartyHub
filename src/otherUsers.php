c086ecdcf1306e1715b43d55aeb07529671740c1
ChiaraGiangiulli <chiara.giangiulli@studio.unibo.it>
2024-01-26 16:55:31 +0100

    modified posts

--- src/otherUsers.php
+++ src/otherUsers.php
@@ -2,7 +2,7 @@
 require_once("database.php");
 $templateParams["titolo"] = "Profile";
 $templateParams["profilo"] = $dbh->getUserFromUsername($_GET['user']);
-$templateParams["js"] = array("js/otherUser.js","js/comments.js");
+$templateParams["js"] = array("js/otherUser.js","js/like.js", "js/comments.js");
 if(isset($_GET['contenuto'])){
     $templateParams["contenutoProfilo"] = $_GET['contenuto'];
     $templateParams["post"] = $dbh->getPostsFromUser($_GET['user']);
--- src/view/profilePost-view.php
+++ src/view/profilePost-view.php
@@ -2,9 +2,32 @@
 <?php 
 foreach ($templateParams["post"] as $post){
 ?>
-<div class="container-sm pt-5">
+<div class="container-sm pt-5 pb-5">
     <div class="row">
-        <img class="img-fluid" src="/PartyHub/src/img/another.jpg" alt="another img">
+        <div class="col-sm-12">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-calendar4-event" viewBox="0 0 16 16">
+            <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5M2 2a1 1 0 0 0-1 1v1h14V3a1 1 0 0 0-1-1zm13 3H1v9a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1z"/>
+            <path d="M11 7.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5z"/>
+        </svg>
+            <a href="event.php?id=<?php print_r($post['idEvento']);?>"><?php print_r($dbh->getEventFromId($post['idEvento'])[0]['Nome']);?></a>
+        </div>
+    </div>
+    <img class="img-fluid" src="/PartyHub/src/img/another.jpg" alt="another img">
+    <div class="row">
+        <div class="col-1">
+        <button type="button" class="btn like-button" id="like" data-postid="<?php echo $post['idPost']; ?>">
+            <?php
+            if(count($dbh->checkLike($_SESSION['userId'], $post['idPost'])) > 0){ ?>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart-fill" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314"/>
+            </svg>
+            <?php } else{ ?>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
+                <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143q.09.083.176.171a3 3 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15"/>
+            </svg>
+            <?php } ?>
+        </button>
+        </div>
         <div class="col-1">
             <div class="offcanvas offcanvas-bottom" id="comments">
                 <div class="offcanvas-header">
@@ -24,7 +47,6 @@ foreach ($templateParams["post"] as $post){
             <p><?php print_r($post['Testo']) ?><p>
         </div>
     </div>
-
 </div>
 <?php }
 ?>
