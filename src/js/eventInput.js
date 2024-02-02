d8cf221ac0cfc0d0da90a395741a72ab9ade78e3
ChiaraGiangiulli <chiara.giangiulli@studio.unibo.it>
2024-02-02 16:02:10 +0100

    event input js

---
+++ src/js/eventInput.js
@@ -0,0 +1,57 @@
+function addChoice() {
+    let choicesContainer = $('#choicesContainer');
+    let newChoiceInput = $('<div class="input-group mb-3">' +
+                            '<input type="text" class="form-control" name="choices[]" placeholder="New Option" required>' +
+                            '<div class="input-group-append">' +
+                                '<button class="btn btn-outline-success" type="button" onclick="removeChoice(this)">Remove</button>' +
+                            '</div>' +
+                            '</div>');
+    choicesContainer.append(newChoiceInput);
+}
+
+
+function removeChoice(button) {
+    $(button).closest('.input-group').remove();
+}
+
+
+function addProduct() {
+        let choicesContainer = $('#productContainer');
+        let newChoiceInput = $('<div class="input-group mb-3">' +
+                                '<input type="text" class="form-control" name="products[]" placeholder="New Product" required>' +
+                                '<input type="text" class="form-control text-end price" name="price[]" required><span class="input-group-text">€</span>' +
+                                '<div class="input-group-append">' +
+                                    '<button class="btn btn-outline-success" type="button" onclick="removeProduct(this)">Remove</button>' +
+                                '</div>' +
+                                '</div>');
+        choicesContainer.append(newChoiceInput);
+        $('.price').last().on('input', function() {
+        var inputValue = this.value;
+        var isValidDecimal = /^-?\d*\.?\d{0,2}$/.test(inputValue);
+
+        if (!isValidDecimal) {
+            this.value = inputValue.slice(0, -1); // Remove the last character (not a valid decimal)
+        }
+    });
+}
+    
+function removeProduct(button) {
+    $(button).closest('.input-group').remove();
+}
+
+document.getElementById('price[]').addEventListener('input', function() {
+    var inputValue = this.value;
+    var isValidDecimal = /^-?\d*\.?\d{0,2}$/.test(inputValue);
+
+    if (!isValidDecimal) {
+        this.value = inputValue.slice(0, -1); // Remove the last character (not a valid decimal)
+    }
+});
+
+document.getElementById("addChoice").addEventListener("click", function() {
+    addChoice();
+});
+
+document.getElementById("addProduct").addEventListener("click", function() {
+    addProduct();
+});
--- src/view/modalsEvent.php
+++ src/view/modalsEvent.php
@@ -3,7 +3,7 @@
     <div class="modal-content">
         <div class="modal-header">
             <h5 class="modal-title" id="list-type">New Survey</h5>
-                    <button type="button" class="close" data-bs-dismiss="modal" aria-hidden="true">&times;</button>
+            <button type="button" class="close" data-bs-dismiss="modal" aria-hidden="true">&times;</button>
         </div>
         <div class="modal-body">
         <div class="container mt-5">
@@ -13,7 +13,7 @@
                 <div class="input-group mb-3">
                 <input type="text" class="form-control" name="choices[]" placeholder="Option" required>
                 <div class="input-group-append">
-                    <button class="btn btn-outline-success" type="button" onclick="addChoice()">Add Choice</button>
+                    <button class="btn btn-outline-success" type="button" id="addChoice">Add Choice</button>
                 </div>
                 </div>
             </div>
@@ -31,7 +31,7 @@
     <div class="modal-content">
         <div class="modal-header">
             <h5 class="modal-title" id="list-type">New Shopping List</h5>
-                    <button type="button" class="close" data-bs-dismiss="modal" aria-hidden="true">&times;</button>
+            <button type="button" class="close" data-bs-dismiss="modal" aria-hidden="true">&times;</button>
         </div>
         <div class="modal-body">
         <div class="container mt-5">
@@ -43,7 +43,7 @@
                 <input type="text" class="form-control text-end" name="price[]" id="price[]" required>
                 <span class="input-group-text">€</span>
                 <div class="input-group-append">
-                    <button class="btn btn-outline-success" type="button" onclick="addProduct()">Add Product</button>
+                    <button class="btn btn-outline-success" type="button" id="addProduct">Add Product</button>
                 </div>
                 </div>
             </div>
@@ -57,56 +57,6 @@
     </div>
     </div>
 </div>
-
-
-<script>
-function addChoice() {
-    let choicesContainer = $('#choicesContainer');
-    let newChoiceInput = $('<div class="input-group mb-3">' +
-                            '<input type="text" class="form-control" name="choices[]" placeholder="New Option" required>' +
-                            '<div class="input-group-append">' +
-                                '<button class="btn btn-outline-success" type="button" onclick="removeChoice(this)">Remove</button>' +
-                            '</div>' +
-                            '</div>');
-    choicesContainer.append(newChoiceInput);
-    }
-
-    function removeChoice(button) {
-    $(button).closest('.input-group').remove();
-    }
-
-    function addProduct() {
-        let choicesContainer = $('#productContainer');
-        let newChoiceInput = $('<div class="input-group mb-3">' +
-                                '<input type="text" class="form-control" name="products[]" placeholder="New Product" required>' +
-                                '<input type="text" class="form-control text-end price" name="price[]" required><span class="input-group-text">€</span>' +
-                                '<div class="input-group-append">' +
-                                    '<button class="btn btn-outline-success" type="button" onclick="removeProduct(this)">Remove</button>' +
-                                '</div>' +
-                                '</div>');
-        choicesContainer.append(newChoiceInput);
-        $('.price').last().on('input', function() {
-        var inputValue = this.value;
-        var isValidDecimal = /^-?\d*\.?\d{0,2}$/.test(inputValue);
-
-        if (!isValidDecimal) {
-            this.value = inputValue.slice(0, -1); // Remove the last character (not a valid decimal)
-        }
-    });
-        }
-    
-        function removeProduct(button) {
-        $(button).closest('.input-group').remove();
-        }
-        document.getElementById('price[]').addEventListener('input', function() {
-            var inputValue = this.value;
-            var isValidDecimal = /^-?\d*\.?\d{0,2}$/.test(inputValue);
-
-            if (!isValidDecimal) {
-                this.value = inputValue.slice(0, -1); // Remove the last character (not a valid decimal)
-            }
-        });
-</script>
 
 <div class="modal fade" id="partecipantsModel" role="dialog" aria-hidden="true">
   <div class="modal-dialog" role="document">
