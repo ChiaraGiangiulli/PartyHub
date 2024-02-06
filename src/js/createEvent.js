6033a0e3494857c39bbba6a5319039b908d0a6cf
emilia <emilia.pace2@studio.unibo.it>
2024-02-06 17:44:28 +0100

    create event

--- src/js/createEvent.js
+++ src/js/createEvent.js
@@ -1,16 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() {
-        document.getElementById("createEvent").addEventListener("click", function(event) {
+        document.getElementById("createEvent").addEventListener("submit", function(event) {
             event.preventDefault();
             let form = document.getElementById("newEventForm");
             let formData = new FormData();
             let inputDate = form.elements["date"].value;
             let inputTime = form.elements["time"].value;
             let inputDateTime = new Date(inputDate + ' ' + inputTime);
-            let now = new Date();
-            if (inputDateTime <= now) {
-                alert("La data dell'evento deve essere successiva alla data corrente.");
-                return;
-            }
             formData.append('name', form.elements["name"].value);
             formData.append('address', form.elements["address"].value);
             formData.append('number', form.elements["number"].value);
@@ -24,13 +19,11 @@ document.addEventListener("DOMContentLoaded", function() {
             axios.post('../api/newEvent.php', formData).then(response => {
                 console.log(response.data);
                 if (response.data.success){
-                    alert(response.data.message);
-                    window.open('/PartyHub/src/profile.php', '_self');
+                    window.open(`/PartyHub/src/event.php?id=${response.data.id}`, '_self');
                 }
                 else{
                     alert(response.data.message);
                 }
-                
             });
         });
 });
